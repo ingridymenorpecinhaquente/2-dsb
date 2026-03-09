97080f5aa4d277a21e088c17f15dc9994d32fcc5
ingridymenorpecinhaquente <moreira.ingridy@escola.pr.gov.br>
2026-03-09 08:59:16 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,57 @@
+// ctrl + s 
+console.log("Olá mundo!")
+// comentario
+// let -> variável local
+//var -> variável global
+let nome = "Ingridy e Emilly "
+let altura = "1.60 e 1.53 "
+let idade = "15 anos "
+let genero = "Mulheres "
+let cor = "Parda e Branca "
+let cabelo = "Moreno com luzes ondulado e Preto ondulado "
+let acessorio = "Brinco e Pulseira "
+let roupa = "Uniforme do colégio "
+let cenario = "Laboratório de informática "
+
+console.log("Olá, somos " + nome + "somos duas " + genero + "temos " + idade + " nossa cor de pele é " + cor + "nossos cabelos são " + cabelo + "estamos usando " + acessorio + "nossas roupas são o " + roupa+ "e estamos no " + cenario)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
